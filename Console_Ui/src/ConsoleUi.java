1733f5509b6a969ac30d4ed59ead0fe3b1ec0f99
Yuval Schlosser <yuvalsl@mta.ac.il>
2020-08-03 15:58:07 +0300

    Revert "i added an int"
    This reverts commit aaccbfc1face0afb418721d4bee09349daa4f78a.
    
    # Conflicts:
    #	Console_Ui/src/ConsoleUi.java

--- Console_Ui/src/ConsoleUi.java
+++ Console_Ui/src/ConsoleUi.java
@@ -1,7 +1,5 @@
 public class ConsoleUi {
     public static void main(String[] args) {
         int i = 5;
-        System.out.println("im in super branch");
-        System.out.println("im in super branch");
     }
 }
