4a027a0ec149db1a879872cacad0c75528442ce8
Dani <dani.zion87@gmail.com>
2020-08-03 19:30:03 +0300

    choice validation created (not cpmpleted)

--- Console_Ui/src/ConsoleUi.java
+++ Console_Ui/src/ConsoleUi.java
@@ -1,14 +1,36 @@
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.util.Scanner;
+
 public class ConsoleUi {
 
     private Menu menu = new Menu();
     public enum Echoice{
-        Yaron ,
-        Dvir
+        readFile,
+        ShowStores,
+        ShowItems,
+        PlaceOrder,
+        ShowHistory
     }
 
-    public void stam(){
+    public int validateChoice(int largestChoiceNumber){
+        System.out.println("Please enter choose number of a commands above(must be between 1 and " + largestChoiceNumber+"):\n");
+        Scanner input = new Scanner(System.in);
+        int numOfChoice = 1;
+        boolean isChoiceValid = false;
+            do {
+                if (input.hasNextInt() && numOfChoice > 0 && numOfChoice <= largestChoiceNumber){
+                    isChoiceValid = true;
+                    numOfChoice = input.nextInt();
+                }
+                else {
+                    System.out.println("Wrong input please try again");
+                    input.next();
+                }
+            }while (isChoiceValid == false);
+            return numOfChoice;
 
-        System.out.println(Echoice.Dvir+" "+Echoice.Yaron.ordinal());
+
     }
 
 }
--- Console_Ui/src/Main.java
+++ Console_Ui/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         ConsoleUi ui = new ConsoleUi();
-
-        ui.stam();
+        int n = ui.validateChoice(6);
+        System.out.println(n);
     }
 }
