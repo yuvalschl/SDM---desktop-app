014f64d0f0ed154d6bf77795abbdfbea92f553f0
Yuval Schlosser <yuvalsl@mta.ac.il>
2020-08-03 15:40:36 +0300

    satom barnch

--- Console_Ui/src/ConsoleUi.java
+++ Console_Ui/src/ConsoleUi.java
@@ -2,5 +2,6 @@ public class ConsoleUi {
     public static void main(String[] args) {
         int i = 5;
         System.out.println("im in super branch");
+        System.out.println("im in super branch");
     }
 }
