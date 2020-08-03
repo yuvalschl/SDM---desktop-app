f5e3aa7d877a64346007562d284099cc102539ec
Dani <dani.zion87@gmail.com>
2020-08-03 17:50:13 +0300

    03.08 17:46

---
+++ Console_Ui/src/Menu.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+public class Menu {
+    private  String menuOption = "<<Super Duper Market>>\n" +
+            "1.Read from file\n"+
+            "2.Show stores\n"+
+            "3.Show items\n"+
+            "4.Place an order\n"+
+            "5.Show order history\n"+
+            "6.EXIT\n"+
+            "Please enter choose number of a commands above:\n";
+
+    public String getMenuOption() {
+        return menuOption;
+    }
+}
