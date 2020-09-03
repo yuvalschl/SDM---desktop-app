ac3a25b2fa6787f6720bfffd1143e9317af878ab
Dani <dani.zion87@gmail.com>
2020-09-03 20:35:14 +0300

    progress bar working (sort of)

--- Ui/src/header/HeaderController.java
+++ Ui/src/header/HeaderController.java
@@ -2,24 +2,30 @@ package header;
 
 import appController.AppController;
 import appController.Main;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+
+import java.util.Optional;
 
 public class HeaderController {
 
 
     private AppController appController;
 
-    @FXML
-    private Button loadXmlButton;
-    @FXML
-    private Button mainMenuButton;
-    @FXML
-    private Text loadActionText;
+    @FXML private Button loadXmlButton;
+    @FXML private Button mainMenuButton;
+    @FXML private Text loadActionText;
+    @FXML private ProgressBar fileProgressBar;
+    @FXML private Label progressPercentText;
 
     public Text getLoadActionText() {
         return loadActionText;
@@ -32,6 +38,7 @@ public class HeaderController {
     @FXML
     void loadXmlAction() {
         appController.loadXmlAction();
+        loadingFileProgress();
         //appController.getXmlLoaded().setValue(false);
     }
 
@@ -40,4 +47,28 @@ public class HeaderController {
         Main.getPrimaryStage().setScene(Main.getMainMenu());
     }
 
+    private void loadingFileProgress(){
+        Task<Void> task = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                for (int i=0 ; i<100; i++) {
+                    Thread.sleep(10);
+                    updateProgress(i, 100);
+                }
+                return null;
+            }
+        };
+        fileProgressBar.progressProperty().unbind();
+        fileProgressBar.progressProperty().bind(task.progressProperty());
+        progressPercentText.textProperty().bind(Bindings.concat(
+                Bindings.format(
+                        "%.0f",
+                        Bindings.multiply(
+                                task.progressProperty(),
+                                100)),
+                " %"));
+        Thread thread = new Thread(task);
+        thread.setDaemon(true);
+        thread.start();
+    }
 }
